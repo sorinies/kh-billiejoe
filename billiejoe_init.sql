614d701d70d7151988a4ccb6187df016a96136da
sorinies <7258136+sorinies@users.noreply.github.com>
2021-07-30 20:34:38 +0900

    초기 설정을 위한 sql 구문 등록
    예약 상태 등록 (반드시 수행해야함)

---
+++ billiejoe_init.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------------------------------
+-- 예약 상태 
+--------------------------------------------------------------------------------
+INSERT INTO reservation_state VALUES(1, '예약중');
+INSERT INTO reservation_state VALUES(2, '대여중');
+INSERT INTO reservation_state VALUES(3, '이용완료');
+INSERT INTO reservation_state VALUES(4, '이용자 취소');
+INSERT INTO reservation_state VALUES(5, '판매자 취소');
