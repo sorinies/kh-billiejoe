1647ec927c02c902eba80e54b686edfaa2283d4a
Hyeseon2 <zxcvzvv@naver.com>
2021-08-09 17:25:38 +0900

    내가 예약한 장소 목록조회 (진행중)

--- billiejoe/src/main/java/kr/co/billiejoe/place/model/dao/PlaceDAO.java
+++ billiejoe/src/main/java/kr/co/billiejoe/place/model/dao/PlaceDAO.java
@@ -2,11 +2,13 @@ package kr.co.billiejoe.place.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.co.billiejoe.place.model.vo.Likes;
+import kr.co.billiejoe.place.model.vo.Pagination;
 import kr.co.billiejoe.place.model.vo.Payment;
 import kr.co.billiejoe.place.model.vo.Place;
 import kr.co.billiejoe.place.model.vo.PlaceAvailable;
@@ -43,6 +45,30 @@ public class PlaceDAO {
 		return session.delete("placeMapper.deleteLike",likes);
 	}
 
+	
+	/**  전체 목록 수 + 예약한 장소 조회
+	 * @param memberNo
+	 * @return
+	 */
+	public Pagination getListCount(int memberNo) {
+		return session.selectOne("placeMapper.getListCount", memberNo);
+	}
+
+	/**  내가 예약한 장소 목록 조회
+	 * @param pagination
+	 * @return placeList
+	 */
+	public List<Place> selectReservationList(Pagination pagination) {
+		
+		int offset = (pagination.getCurrentPage() -1 ) * pagination.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		// offset 만큼 건너 뛰고, limit만큼의 행을 얻어옴
+		
+		return session.selectList("placeMapper.selectReservationList", pagination, rowBounds);
+		
+	}
+		
 	public int getReserveNo() {
 		// TODO Auto-generated method stub
 		return session.selectOne("placeMapper.getReserveNo", null);
--- billiejoe/src/main/java/kr/co/billiejoe/place/model/service/PlaceServiceImpl.java
+++ billiejoe/src/main/java/kr/co/billiejoe/place/model/service/PlaceServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.billiejoe.place.model.dao.PlaceDAO;
 import kr.co.billiejoe.place.model.vo.Likes;
+import kr.co.billiejoe.place.model.vo.Pagination;
 import kr.co.billiejoe.place.model.vo.Payment;
 import kr.co.billiejoe.place.model.vo.Place;
 import kr.co.billiejoe.place.model.vo.PlaceAvailable;
@@ -91,6 +92,25 @@ public class PlaceServiceImpl implements PlaceService{
 	public int deleteLike(Likes likes) {
 		return dao.deleteLike(likes);
 	}
+	
+	// 전체 목록 수 + 예약한 장소 조회
+		@Override
+		public Pagination getPagination(Pagination pg, int memberNo) {
+			
+			// 1) 전체 목록 수 조회
+			Pagination selectPg = dao.getListCount(memberNo);
+			
+			
+			// 2) 계산이 완료된 Pagination 객체 생성 후 반환
+			return new Pagination(pg.getCurrentPage(), selectPg.getListCount()) ;
+		}
+		
+		// 내가 예약한 목록 조회
+		@Override
+		public List<Place> selectReservationList(Pagination pagination) {
+			return dao.selectReservationList(pagination);
+		}
+			
 	/**장소예약하기 
 	 *
 	 */
